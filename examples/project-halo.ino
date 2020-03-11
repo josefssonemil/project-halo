e1b6b3d2f6aa7c2ca9906d52ec6cfe04571653ac
Emil Josefsson <emiljos@student.chalmers.se>
2020-03-11 19:11:13 +0100

    Update project-halo.ino

--- examples/project-halo.ino
+++ examples/project-halo.ino
@@ -1,50 +1,28 @@
 
-/* 
-Source code for Project Halo - a lamp designed in the course Tangible Interaction.
-Uses three components:
-- SOFTPOT: For sensing movement throughout the ring
-https://learn.sparkfun.com/tutorials/softpot-hookup-guide/all
-1) Set timer
-  - Map to LED LIGHTS (2)
-
-- LED STRIP: Lights
-https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
-
-1) Startup
-  - Indicate that it is "ready" by lighting up all individual lights sequentially in different colors.
-  - Pulsate
-  - Wait
-  - Turn them off
-
-2) Timer set
-  - Map softpot values to lights, lighting up all the lights in that range
-  - When user stops pushing softpot, wait a delay in case of a slip
-  - Pulsate the enabled lights, maybe in different colors or something like that
-  - Start timer
-
-3) Timer finished
-   - Light up all lights and pulsate them to indiciate that the timer is out
-
-- Distance Sensor: To sense turn on / turn off
-https://create.arduino.cc/projecthub/jenniferchen/distance-measuring-sensor-900520
-
-1) Wait for startup
- - Wait for the value to pass the threshold value
- - Pass to LED STRIPS (1)
-2) Wait for turn off
-  - Turn off all LED STRIPS lights
+/*
+  Source code for Project Halo - a lamp designed in the course Tangible Interaction.
+  Uses three components:
+  - SOFTPOT: For sensing movement throughout the ring
+  https://learn.sparkfun.com/tutorials/softpot-hookup-guide/all
 
 
+  - LED STRIP: Lights
+  https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
+
+
+  - Distance Sensor: To sense turn on / turn off
+  https://create.arduino.cc/projecthub/jenniferchen/distance-measuring-sensor-900520
 */
-
 // COMPONENT SETUP //
 
 // -- LED STRIPS
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 #include "FastLED.h"
+
+
 
 
 // Which pin on the Arduino is connected to the NeoPixels?
@@ -52,29 +30,41 @@ https://create.arduino.cc/projecthub/jenniferchen/distance-measuring-sensor-9005
 #define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 31
+#define LED_COUNT 29
+
+#define MIN_BRIGHTNESS 10                   // watch the power!
+#define MAX_BRIGHTNESS 120                   // watch the power!
+
+
+int fadeAmount = 5;  // Set the amount to fade I usually do 5, 10, 15, 20, 25 etc even up to 255.
+int brightness = 0;
+
+
 
 int sessions[100];
 int sessionActive = 0;
-int lightsInSession;
 
-CRGB leds[LED_COUNT];
-
-
-
-// Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 int lightsOn = 0;
 
-const uint32_t red = strip.Color(255,   0,   0);
+
+CRGB leds[LED_COUNT];
+int lightsInSession = 0;
+
+CRGB ACTIVE_COLOR = CRGB::Red;
+uint8_t hue = 0;
 
 
-// -- SOFTPOT 
+
+
+// -- SOFTPOT
 const int SOFT_POT_PIN = A0; // Pin connected to softpot wiper, used for setting timer
 
 const int GRAPH_LENGTH = 40; // Length of line graph
 
 const int SOFT_POT_PIN_2 = A2; // Pin connected to the second softpot wiper, used for changing colors
+
+int softPotADC;
+int softPotADC_2;
 
 
 // -- IR Distance Sensor
@@ -84,76 +74,131 @@ int distanceVal = 0;
 // if above val + threshold, we light it up
 const int THRESHOLD = 100;
 //roughly the default measuring value
-const int STANDARD_VAL = 330;
+const int STANDARD_VAL = 100;
+
+const int TOLERANCE = 250;
 
 
-void setup() 
+
+unsigned long previousMillis = 0;    // will store last time LED was updated
+
+
+long currentMillis;
+long second = 1000;
+long sessionLength = 10000;
+unsigned long startMillis;  //some global variables available anywhere in the program
+
+
+
+int currentPixel;
+
+
+void setup()
 {
+
   Serial.begin(19200);
   pinMode(SOFT_POT_PIN, INPUT);
+  pinMode(SOFT_POT_PIN_2, INPUT);
 
-  
-
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  FastLED.addLeds<NEOPIXEL, 6>(leds, LED_COUNT);
 
   pinMode(distancePin, INPUT);
 
-  startUpLights();
+  FastLED.clear();
+
+  for (int i = 0; i <= LED_COUNT; i++) {
+
+    leds[i] = ACTIVE_COLOR;
+  }
+
+  FastLED.show();
+
+  lightsInSession = LED_COUNT;
+
+
+
 }
 
-void loop() 
+void loop()
 {
+
+  currentMillis = millis();
+
   // Read in the soft pot's ADC value
-  int softPotADC = analogRead(SOFT_POT_PIN);
-  int softPotADC_2 = analogRead(SOFT_POT_PIN_2);
-  // Map the 0-1023 value to 0-40
-  int softPotPosition = map(softPotADC, 0, 1023, 0, GRAPH_LENGTH);
-  int softPotPosition_2 = map(softPotADC_2, 0, 1023, 0, GRAPH_LENGTH);
+  softPotADC = analogRead(SOFT_POT_PIN);
+  softPotADC_2 = analogRead(SOFT_POT_PIN_2);
 
-  if(lightsOn == 1){
-    fillLights(softPotADC, softPotADC_2);
+  int distanceVal = analogRead(distancePin);
+
+
+
+  if (softPotADC > 8) {
+    fillLights(softPotADC);
   }
 
-  // Print a line graph:
-  Serial.print("<"); // Starting end
-  for (int i=0; i<GRAPH_LENGTH; i++)
-  {
-    if (i == softPotPosition) Serial.print("|");
-    else Serial.print("-");
+  if (softPotADC_2 > 1) {
+    setColors(softPotADC_2);
   }
-  Serial.println("> (" + String(softPotADC) + ")");
 
 
-  //delay(500);
 
-  senseOnOff();
+  if (sessionActive == 1) {
+    if (currentMillis - startMillis >= second) {
+
+      turnOffOneLED();
+      startMillis = currentMillis;
+
+      if (lightsInSession == 0) {
+        sessionActive = 0;
+      }
+
+
+    }
+  }
+  else {
+    pulsate2();
+
+    if (softPotADC_2 > 1) {
+      setColors(softPotADC_2);
+    }
+  }
+
+
 }
 
 
-void fillLights(int softpotValue, int softpotValue2){
-  int combinedValue = softpotValue + softpotValue2;
-   
+void fillLights(int softpotValue) {
+
   //softpot value is 0-1023
   //ledcount
-  int threshold = 1023 * 2 / LED_COUNT;
+  int threshold = 1023 / LED_COUNT;
 
   // calc number of lights to turn on
-  int lights = combinedValue / threshold;
-  lightsInSession = lights;
-  
-  for (int i=0; i <= lights; i++){
-    strip.setPixelColor(i, red);
-    strip.show();
+  int lights = softpotValue / threshold;
+  lightsInSession = lights + 1;
+
+  FastLED.clear();
+  for (int i = 0; i <= lightsInSession; i++) {
+
+    leds[i] = ACTIVE_COLOR;
   }
 
- 
+
+
+  FastLED.show();
+
+
+  startSession(lights);
 }
 
-int findLastIndex(){
-  for(int i = 0; i < sizeof(sessions); i++){
-    if (sessions[i] == 0){
+
+
+
+
+
+int findLastIndex() {
+  for (int i = 0; i < sizeof(sessions); i++) {
+    if (sessions[i] == 0) {
       return i;
     }
   }
@@ -162,113 +207,121 @@ int findLastIndex(){
 }
 
 
-void startSession(int lights){
+void startSession(int lights) {
   int minutes = lights * 2;
   int seconds = minutes * 60;
 
   int timeLeft = seconds;
 
-  sessionActive = 1;
+  //sessionActive = 1;
 
   int index = findLastIndex();
-  if(index != 101){
+  if (index != 101) {
     sessions[index] = seconds;
 
-  runSession(seconds, lights);
+    currentPixel = lights + 1;
+
+    sessionActive = 1;
+
   }
-  
-  
+
 }
 
-void runSession(int seconds, int lights){
-  int timer = seconds;
-  
-  int currentPixel = lights; 
-  while (seconds > 0){
-    delay(1000);
-    seconds = seconds - 1;
 
-    //each light is worth 2 minutes
-    if (seconds % 120 == 0){
-      strip.setPixelColor(currentPixel, red);
-      currentPixel = currentPixel - 1;
-      strip.show();
-    }
+void turnOffOneLED() {
+
+
+  Serial.println("Turning off one led");
+  leds[currentPixel] = CRGB::Black;
+  lightsInSession = lightsInSession - 1;
+  currentPixel = currentPixel - 1;
+  FastLED.show();
+}
+
+
+void setColors(int softpotValue) {
+
+  int mapped = map(softpotValue, 0, 1023, 0, 255);
+  CRGB color = CHSV(mapped, 255, 255);
+
+  FastLED.clear();
+  for (int i = 0; i < lightsInSession; i++) {
+    leds[i] = color;
+    ACTIVE_COLOR = color;
+
   }
-  
+
+  FastLED.show();
+
+
 }
 
-void setColors(int softpotValue){
-  
+
+void pulsate() {
+  int hue = 0;
+  int divisor = 30;
+
+  float breath = (exp(sin(millis() / 5000.0 * PI)) - 0.36787944) * 108.0;
+  breath = map(breath, 0, 255, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
+  FastLED.setBrightness(breath);
+  fill_rainbow(leds, LED_COUNT, (hue++ / divisor));
+  if (hue == (255 * divisor)) {
+    hue = 0;
+  }
+  FastLED.show();
+
+  FastLED.setBrightness(MAX_BRIGHTNESS);
+
+  FastLED.show();
+
 }
 
 
 
-void showHistory(){
+void showHistory() {
   int totalseconds;
-  for(int i = 0; i< (sizeof(sessions) / sizeof(sessions[0])); i++){
+  for (int i = 0; i < (sizeof(sessions) / sizeof(sessions[0])); i++) {
     totalseconds = totalseconds + sessions[i];
   }
 
   int average = totalseconds / (sizeof(sessions) / sizeof(sessions[0]));
 
-  for(int i = 0; i < LED_COUNT; i++){
-    strip.setPixelColor(i, red);
+  for (int i = 0; i < LED_COUNT; i++) {
+    leds[i] = ACTIVE_COLOR;
   }
 
-  strip.show();
+  FastLED.show();
 }
 
 
-void startUpLights(){
-  lightsOn = 1;
-  
-    for (int i = 0; i < strip.numPixels(); i++){
-      strip.setPixelColor(i, red);
-    }
-    
-    // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
- /* for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      // Offset pixel hue by an amount to make one full revolution of the
-      // color wheel (range of 65536) along the length of the strip
-      // (strip.numPixels() steps):
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-      // optionally add saturation and value (brightness) (each 0 to 255).
-      // Here we're using just the single-argument hue variant. The result
-      // is passed through strip.gamma32() to provide 'truer' colors
-      // before assigning to each pixel:
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }*/
-    strip.show(); // Update strip with new contents
-    delay(1000);  // Pause for a moment
-  
+void pulsate2() {
+  for (int i = 0; i < LED_COUNT; i++ )
+  {
+    leds[i] = ACTIVE_COLOR;  // Set Color HERE!!!
+    leds[i].fadeLightBy(brightness);
+  }
+  FastLED.show();
+  brightness = brightness + fadeAmount;
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness == 0 || brightness == 255)
+  {
+    fadeAmount = -fadeAmount ;
+  }
+  delay(65);  // This delay sets speed
 }
 
-void turnOffLights(){
+
+void turnOffLights() {
   lightsOn = 0;
-  strip.clear();
-  delay(1000);
+  FastLED.clear();
 }
 
-void senseOnOff(){
-  distanceVal = analogRead(distancePin); 
-  Serial.println(distanceVal);
+void senseOff() {
 
-  if (STANDARD_VAL + THRESHOLD > 400){
-    if (lightsOn == 0){
-      //startUpLights();
-    }
+  if (STANDARD_VAL + THRESHOLD > 400) {
+    //turnOffLights();
 
-    else {
-      //turnOffLights();
-    }
+
   }
 
 }
-
-
